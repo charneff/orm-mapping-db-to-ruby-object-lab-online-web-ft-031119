a8266a2fb41a80189d660d30c0aefb3fba432e2b
Charlotte Neff <charlotte.neff@gmail.com>
2019-04-14 01:46:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,7 +21,7 @@ class Student
 
   def self.find_by_name(name)
    sql = <<-SQL
-    # return a new instance of the Student class	    SELECT * 
+    SELECT * 
     FROM students
     WHERE name = ?
     LIMIT 1
